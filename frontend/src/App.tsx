b6159ad338441e1b0c170653bf526742c89b24d8
Aisheel Nair <aisheelnair@outlook.com>
2026-02-03 18:20:42 +0200

    chore: add frontend scaffold

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="h-screen bg-blue-400 font-josefin-sans">
+      <div className=" h-full text-4xl font-medium flex items-center justify-center">
+        Aisheel's Portfolio
+      </div>
+    </div>
+  );
+}
